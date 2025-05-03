d5535a255280c439036ddbe6318595f0a943428c
Matheus Silva <matpaixaoo@outlook.com>
2025-05-03 14:52:48 -0300

    build: version 1.1.0
    Final commit of the refactoring process. It addresses the upgrading of the dependencies and a small fix.

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,10 +3,16 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from './schemas/user.schema';
+import { Content, ContentSchema } from '../contents/schemas/content.schema';
+import { ContentsModule } from 'src/contents/contents.module';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    MongooseModule.forFeature([
+      { name: User.name, schema: UserSchema },
+      { name: Content.name, schema: ContentSchema }
+    ]),
+    ContentsModule
   ],
   controllers: [UsersController],
   providers: [UsersService],
