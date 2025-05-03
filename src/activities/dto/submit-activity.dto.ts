9ac28a1a7cf0954717087db4d35db4a7300ac445
Matheus Silva <matpaixaoo@outlook.com>
2025-05-03 14:44:42 -0300

    remove: comments and activities
    The comments module has been removed from the project as it was not being used. The activities module has also been removed because it is going to be a part of the contents module.

--- src/activities/dto/submit-activity.dto.ts
+++
@@ -1,9 +0,0 @@
-import { ApiProperty } from '@nestjs/swagger';
-import { IsMongoId, IsNotEmpty } from 'class-validator';
-
-export class SubmitActivityDto {
-  @ApiProperty({ description: '', example: '60c72b2f5b8f9c001f9f1a23' })
-  @IsMongoId()
-  @IsNotEmpty()
-  user_id: string;
-}
--- src/app.module.ts
+++ src/app.module.ts
@@ -9,9 +9,7 @@ import databaseConfig from './database/config/database.config';
 import { LoggerMiddleware } from './utils/middlewares/logger.middleware';
 import { AuthModule } from './auth/auth.module';
 import { UnitsModule } from './units/units.module';
-import { ActivitiesModule } from './activities/activities.module';
 import { ContentsModule } from './contents/contents.module';
-import { CommentsModule } from './comments/comments.module';
 import appConfig from './config/app.config';
 import authConfig from './auth/config/auth.config';
 
@@ -26,9 +24,7 @@ import authConfig from './auth/config/auth.config';
     UsersModule,
     AuthModule,
     UnitsModule,
-    ActivitiesModule,
     ContentsModule,
-    CommentsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
